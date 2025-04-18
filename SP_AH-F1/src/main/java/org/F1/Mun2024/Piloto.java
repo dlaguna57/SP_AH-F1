f004bfb259369e73bd4492dc5993d2b3f88cbdde
StivenPST <edwinpasto3@gmail.com>
2025-04-18 17:45:48 -0500

    Update Piloto.java

--- SP_AH-F1/src/main/java/org/F1/Mun2024/Piloto.java
+++ SP_AH-F1/src/main/java/org/F1/Mun2024/Piloto.java
@@ -13,7 +13,7 @@ public class Piloto {
     Circuito posiciones;
 
     // Definicion de Constructores
-    public Piloto(){
+    public Piloto() {
     }
 
     public Piloto(String nameP, Equipo equipo, int edad, String nacionalidad, int campGanadosP, int carrerasDispF1, int puntosAcum2024P) {
@@ -25,21 +25,22 @@ public class Piloto {
         this.carrerasDispF1 = carrerasDispF1;
         this.puntosAcum2024P = puntosAcum2024P;
     }
-    public void printPiloto(List<Piloto> pilotos){
+
+    public void printPiloto(List<Piloto> pilotos) {
         System.out.println("**** Pilotos ****\n");
         int countPil = 1;
         for (Piloto piloto : pilotos) {
-            System.out.println("["+countPil+"] Piloto: "+piloto.nameP+" Equipo: "+piloto.equipo);
+            System.out.println("[" + countPil + "] Piloto: "+piloto.nameP + " Equipo: "+piloto.equipo.nameEqu);
             countPil++;
         }
     }
 
     // Definicion de Metodo de Impresion
-    public void printInfPilotos(Piloto  piloto) {
+    public void printInfPilotos(Piloto piloto) {
         System.out.println("**** Informacion de Pilotos ****\n");
-            System.out.println("Nombre y apellido: "+piloto.nameP+"\nEquipo: "+piloto.equipo.nameEqu+"\nEdad: "+piloto.edad);
-            System.out.println("Nacionalidad: "+piloto.nacionalidad+"\n# Campeonatos Ganados: "+piloto.campGanadosP);
-            System.out.println("# Carreras Disputadas en F1: "+piloto.carrerasDispF1+"\nPuntos Acumulados 2024: "+piloto.puntosAcum2024P);
-            System.out.println();
-        }
- 
+        System.out.println("Nombre y apellido: " + piloto.nameP + "\nEquipo: " + piloto.equipo.nameEqu + "\nEdad: " + piloto.edad);
+        System.out.println("Nacionalidad: " + piloto.nacionalidad + "\n# Campeonatos Ganados: " + piloto.campGanadosP);
+        System.out.println("# Carreras Disputadas en F1: " + piloto.carrerasDispF1 + "\nPuntos Acumulados 2024: " + piloto.puntosAcum2024P);
+        System.out.println();
+    }
+}
