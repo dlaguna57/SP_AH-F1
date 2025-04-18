e546bbd8aed5f12eab85c8061ed9ad79785afcc3
StivenPST <edwinpasto3@gmail.com>
2025-04-18 00:43:35 -0500

    Update Equipo.java

--- SP_AH-F1/src/main/java/org/F1/Mun2024/Equipo.java
+++ SP_AH-F1/src/main/java/org/F1/Mun2024/Equipo.java
@@ -16,6 +16,16 @@ public class Equipo {
     public Equipo(){
     }
 
+    public Equipo(String nameEqu, String director, String paisOrig, int campGanadosEqu, int puntosAcumMun2024, String pOne, String pTwo) {
+        this.nameEqu = nameEqu;
+        this.director = director;
+        this.paisOrig = paisOrig;
+        this.campGanadosEqu = campGanadosEqu;
+        this.puntosAcumMun2024 = puntosAcumMun2024;
+        this.pOne = pOne;
+        this.pTwo = pTwo;
+    }
+
     public Equipo(String nameEqu, String director, String paisOrig, int campGanadosEqu, int puntosAcumMun2024, String pOne, String pTwo, String extra) {
         this.nameEqu = nameEqu;
         this.director = director;
@@ -30,9 +40,9 @@ public class Equipo {
     public void printEquipos(List<Equipo> equipos){
         System.out.println("**** Informacion de Equipos ****");
         for (Equipo equipo : equipos) {
-                System.out.println("Nombre de equipo: "+equipo.nameEqu+"\nDirector general: "+equipo.director+"\nPais de origen: "+equipo.paisOrig);
-                System.out.println("# Campeonatos ganados: "+equipo.campGanadosEqu+"\nPuntos acumlados Mundial 2024: "+equipo.puntosAcumMun2024);
-                System.out.println("Piloto 1: "+equipo.pOne+"\nPiloto 2: "+equipo.pTwo);
+            System.out.println("Nombre de equipo: "+equipo.nameEqu+"\nDirector general: "+equipo.director+"\nPais de origen: "+equipo.paisOrig);
+            System.out.println("# Campeonatos ganados: "+equipo.campGanadosEqu+"\nPuntos acumlados Mundial 2024: "+equipo.puntosAcumMun2024);
+            System.out.println("Piloto 1: "+equipo.pOne+"\nPiloto 2: "+equipo.pTwo);
             if (equipo.extra != null) {
                 System.out.println("Piloto Extra: " + equipo.extra);
             }
