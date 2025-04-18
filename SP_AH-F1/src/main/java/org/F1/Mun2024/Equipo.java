d5427f76501419c534bd8cfebef70139347666b7
StivenPST <edwinpasto3@gmail.com>
2025-04-18 03:21:12 -0500

    Update Equipo.java

--- SP_AH-F1/src/main/java/org/F1/Mun2024/Equipo.java
+++ SP_AH-F1/src/main/java/org/F1/Mun2024/Equipo.java
@@ -8,43 +8,38 @@ public class Equipo {
     String paisOrig;
     int campGanadosEqu;
     int puntosAcumMun2024;
-    String pOne;
-    String pTwo;
-    String extra;
+    Piloto pOne;
+    Piloto pTwo;
+    Piloto extra;
 
-
+    // Definicion de Constructores
     public Equipo(){
     }
 
-    public Equipo(String nameEqu, String director, String paisOrig, int campGanadosEqu, int puntosAcumMun2024, String pOne, String pTwo) {
+    public Equipo(String nameEqu, String director, String paisOrig, int campGanadosEqu, int puntosAcumMun2024) {
         this.nameEqu = nameEqu;
         this.director = director;
         this.paisOrig = paisOrig;
         this.campGanadosEqu = campGanadosEqu;
         this.puntosAcumMun2024 = puntosAcumMun2024;
-        this.pOne = pOne;
-        this.pTwo = pTwo;
     }
 
-    public Equipo(String nameEqu, String director, String paisOrig, int campGanadosEqu, int puntosAcumMun2024, String pOne, String pTwo, String extra) {
-        this.nameEqu = nameEqu;
-        this.director = director;
-        this.paisOrig = paisOrig;
-        this.campGanadosEqu = campGanadosEqu;
-        this.puntosAcumMun2024 = puntosAcumMun2024;
+    // Definicion Metodo de asignacion de Pilotos
+    public void setPilotos(Piloto pOne, Piloto pTwo, Piloto extra) {
         this.pOne = pOne;
         this.pTwo = pTwo;
         this.extra = extra;
     }
 
+    // Definicion Metodo de Impresion
     public void printEquipos(List<Equipo> equipos){
-        System.out.println("**** Informacion de Equipos ****");
+        System.out.println("**** Informacion de Equipos ****\n");
         for (Equipo equipo : equipos) {
             System.out.println("Nombre de equipo: "+equipo.nameEqu+"\nDirector general: "+equipo.director+"\nPais de origen: "+equipo.paisOrig);
             System.out.println("# Campeonatos ganados: "+equipo.campGanadosEqu+"\nPuntos acumlados Mundial 2024: "+equipo.puntosAcumMun2024);
-            System.out.println("Piloto 1: "+equipo.pOne+"\nPiloto 2: "+equipo.pTwo);
+            System.out.println("Piloto 1: "+equipo.pOne.nameP+"\nPiloto 2: "+equipo.pTwo.nameP);
             if (equipo.extra != null) {
-                System.out.println("Piloto Extra: " + equipo.extra);
+                System.out.println("Piloto Extra: " + equipo.extra.nameP);
             }
             System.out.println();
         }
