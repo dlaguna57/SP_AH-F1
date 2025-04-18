efb283124c1a22b97a645aafbed00ff7a0fdc5bd
StivenPST <edwinpasto3@gmail.com>
2025-04-18 03:05:15 -0500

    Update Piloto.java

--- SP_AH-F1/src/main/java/org/F1/Mun2024/Piloto.java
+++ SP_AH-F1/src/main/java/org/F1/Mun2024/Piloto.java
@@ -1,35 +1,39 @@
 package org.F1.Mun2024;
-import org.F1.Mun2024.Equipo;
+
+import java.util.List;
 
 public class Piloto {
     String nameP;
+    Equipo equipo;
     int edad;
     String nacionalidad;
-    Equipo equipo;
     int campGanadosP;
     int carrerasDispF1;
     int puntosAcum2024P;
     Circuito posiciones;
 
+    // Definicion de Constructores
     public Piloto(){
+    }
 
-    }
-    public Piloto(String nameP) {
+    public Piloto(String nameP, Equipo equipo, int edad, String nacionalidad, int campGanadosP, int carrerasDispF1, int puntosAcum2024P) {
         this.nameP = nameP;
-    }
-    public Piloto(String nameP, int edad, String nacionalidad, Equipo equipo, int campGanadosP, int carrerasDispF1, int puntosAcum2024P, Circuito posiciones) {
-        this.nameP = nameP;
+        this.equipo = equipo;
         this.edad = edad;
         this.nacionalidad = nacionalidad;
-        this.equipo = equipo;
         this.campGanadosP = campGanadosP;
         this.carrerasDispF1 = carrerasDispF1;
         this.puntosAcum2024P = puntosAcum2024P;
-        this.posiciones = new Circuito();
     }
 
-
-    public String getNameP() {
-        return nameP;
+    // Definicion de Metodo de Impresion
+    public void printPilotos(List<Piloto> pilotos) {
+        System.out.println("**** Informacion de Pilotos ****\n");
+        for (Piloto piloto : pilotos) {
+            System.out.println("Nombre y apellido: "+piloto.nameP+"\nEquipo: "+piloto.equipo.nameEqu+"\nEdad: "+piloto.edad);
+            System.out.println("Nacionalidad: "+piloto.nacionalidad+"\n# Campeonatos Ganados: "+piloto.campGanadosP);
+            System.out.println("# Carreras Disputadas en F1: "+piloto.carrerasDispF1+"\nPuntos Acumulados 2024: "+piloto.puntosAcum2024P);
+            System.out.println();
+        }
     }
 }
