eaf35c1e0d8281d4a3f10345668224b262da0a50
StivenPST <edwinpasto3@gmail.com>
2025-05-19 14:20:34 -0500

    Update Private Get y Set In Piloto.java

--- SP_AH-F1/src/main/java/org/F1/Mun2024/Piloto.java
+++ SP_AH-F1/src/main/java/org/F1/Mun2024/Piloto.java
@@ -3,18 +3,16 @@ package org.F1.Mun2024;
 import java.util.List;
 
 public class Piloto {
-    String nameP;
-    Equipo equipo;
-    int edad;
-    String nacionalidad;
-    int campGanadosP;
-    int carrerasDispF1;
-    int puntosAcum2024P;
-    Circuito posiciones;
+    private String nameP;
+    private Equipo equipo;
+    private int edad;
+    private String nacionalidad;
+    private int campGanadosP;
+    private int carrerasDispF1;
+    private int puntosAcum2024P;
 
     // Definicion de Constructores
-    public Piloto() {
-    }
+    public Piloto() {    }
 
     public Piloto(String nameP, Equipo equipo, int edad, String nacionalidad, int campGanadosP, int carrerasDispF1, int puntosAcum2024P) {
         this.nameP = nameP;
@@ -26,11 +24,30 @@ public class Piloto {
         this.puntosAcum2024P = puntosAcum2024P;
     }
 
+    // Definicion de metodos Get
+    public String getNameP() {return nameP;}
+    public Equipo getEquipo() {return equipo;}
+    public int getEdad() {return edad;}
+    public String getNacionalidad() {return nacionalidad;}
+    public int getCampGanadosP() {return campGanadosP;}
+    public int getCarrerasDispF1() {return carrerasDispF1;}
+    public int getPuntosAcum2024P() {return puntosAcum2024P;}
+
+    // Definicion de metodos Set
+    public void setNameP(String nameP) {this.nameP = nameP;}
+    public void setEquipo(Equipo equipo) {this.equipo = equipo;}
+    public void setEdad(int edad) {this.edad = edad;}
+    public void setNacionalidad(String nacionalidad) {this.nacionalidad = nacionalidad;}
+    public void setCampGanadosP(int campGanadosP) {this.campGanadosP = campGanadosP;}
+    public void setCarrerasDispF1(int carrerasDispF1) {this.carrerasDispF1 = carrerasDispF1;}
+    public void setPuntosAcum2024P(int puntosAcum2024P) {this.puntosAcum2024P = puntosAcum2024P;}
+
+
     public void printPiloto(List<Piloto> pilotos) {
         System.out.println("**** Pilotos ****\n");
         int countPil = 1;
         for (Piloto piloto : pilotos) {
-            System.out.println("[" + countPil + "] Piloto: "+piloto.nameP + " Equipo: "+piloto.equipo.nameEqu);
+            System.out.println("[" + countPil + "] Piloto: "+piloto.getNameP() + " Equipo: "+piloto.getEquipo().getNameEqu());
             countPil++;
         }
     }
@@ -38,9 +55,9 @@ public class Piloto {
     // Definicion de Metodo de Impresion
     public void printInfPilotos(Piloto piloto) {
         System.out.println("**** Informacion de Pilotos ****\n");
-        System.out.println("Nombre y apellido: " + piloto.nameP + "\nEquipo: " + piloto.equipo.nameEqu + "\nEdad: " + piloto.edad);
-        System.out.println("Nacionalidad: " + piloto.nacionalidad + "\n# Campeonatos Ganados: " + piloto.campGanadosP);
-        System.out.println("# Carreras Disputadas en F1: " + piloto.carrerasDispF1 + "\nPuntos Acumulados 2024: " + piloto.puntosAcum2024P);
+        System.out.println("Nombre y apellido: " + piloto.getNameP() + "\nEquipo: " + piloto.getEquipo().getNameEqu() + "\nEdad: " + piloto.getEdad());
+        System.out.println("Nacionalidad: " + piloto.getNacionalidad() + "\n# Campeonatos Ganados: " + piloto.getCampGanadosP());
+        System.out.println("# Carreras Disputadas en F1: " + piloto.getCarrerasDispF1() + "\nPuntos Acumulados 2024: " + piloto.getPuntosAcum2024P());
         System.out.println();
     }
 }
