d32861ee03f91f1535f7b18a8e0c8c7eb456b009
StivenPST <edwinpasto3@gmail.com>
2025-05-19 14:22:36 -0500

    Update Private Get y Set In Sprint.java

--- SP_AH-F1/src/main/java/org/F1/Mun2024/Sprint.java
+++ SP_AH-F1/src/main/java/org/F1/Mun2024/Sprint.java
@@ -3,11 +3,9 @@ package org.F1.Mun2024;
 import java.util.List;
 
 public class Sprint extends Circuito{
-    public Sprint(){
-    }
+    public Sprint(){    }
 
     public Sprint(String nameCirc, String paisCirc, String fecha, String longCirc){
         super(nameCirc,paisCirc,fecha,longCirc);
     }
-
 }
